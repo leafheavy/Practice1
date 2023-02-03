dad09e6d2729e8c53dfd9bb78be2e9c889fb9a86
leafheavy <1552354040@qq.com>
2023-02-03 13:21:51 +0800

    正则表达式代码练习

---
+++ practice8.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+import re
+# str = "Python is my favorite language!"
+# result = re.match('Python i', str)
+# print(result.group())
+# result1 = re.match('p', str, re.I)
+# print(result1.group())
+
+# data = "abcdaaa"
+# parrtern = '.....'
+# res1 = re.match(parrtern, data)
+# print(res1.group())
+# names = "peter", "jack", "mark", "pekee"
+# pattern = 'p....'
+# for name in names:
+#     res2 = re.match('p....', name)
+#     if res2:
+#         print(res2.group())
+
+# str1 = "helloldl"
+# res = re.match('[he]+.*[d]', str1)
+# print(res.group())
+
+# reobj = re.compile('\d{4}')
+# rs = reobj.match("1234")
+# print(rs.group())
+
+# rs = re.match('\d{6,9}', '111222333')
+# print(rs.group())
+# rs1 = re.match('\d{6,9}?', '111222333')
+# print(rs1.group())
+
+# User = "111222333"
+# user = re.compile(User)
+# code = input("请输入你的用户密码:")
+# if user.match(code):
+#     print("匹配成功！进入系统")
+# else:
+#     print("密码错误，请重新输入")
+
+# User = "[a-zA-Z]{6,18}"
+# user = re.compile(User)
+# code = input("请输入你的用户名:")
+# if user.match(code):
+#     print("创建成功！进入系统")
+# else:
+#     print("用户名格式错误，请重新输入（6~18位英文字母组成）")
